f13ae7b0bb9b26f7fb52451536b3c77faae6448a
VeronicaVaras <varas9222@gmail.com>
2020-10-09 23:39:09 -0500

    cambios

--- FORMULARIO/Controllers/Formulario.cs
+++ FORMULARIO/Controllers/Formulario.cs
@@ -23,15 +23,12 @@ namespace FORMULARIO.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        [HttpPost]
+        public IActionResult Enviar(Formulario objFormulario){
+            objFormulario.Respuesta = "Ya estas registrado" + objFormulario.Nombre;
+            return View("index", objFormulario);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
--- FORMULARIO/Models/Formulario.cs
+++ FORMULARIO/Models/Formulario.cs
@@ -4,13 +4,12 @@ namespace FORMULARIO.Models
 {
     public class Formulario
     {
-        public string nombre { get; set; }
-        public string apellido { get; set; }
-        public string email { get; set; }
-        public int telefono { get; set; }
-        public string subject { get; set; }
-        public string mensaje { get; set; }
-        public string enviar { get; set; }
-        public string respuesta { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Subject { get; set; }
+        public string Mensaje { get; set; }
+        public string Respuesta { get; set; }
     }
 }
