d663655ab2e86882beb5a723835591d7bad31463
VeronicaVaras <varas9222@gmail.com>
2020-10-09 22:51:32 -0500

    formulario

---
+++ FORMULARIO/Models/Formulario.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace FORMULARIO.Models
+{
+    public class Formulario
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public int telefono { get; set; }
+        public string subject { get; set; }
+        public string mensaje { get; set; }
+        public string enviar { get; set; }
+        public string respuesta { get; set; }
+    }
+}
