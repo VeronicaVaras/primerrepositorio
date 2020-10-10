0d8bc701fb58a270b1516e85b77aa026e1b1183f
VeronicaVaras <varas9222@gmail.com>
2020-10-10 00:42:21 -0500

    nuevo

---
+++ FormMvc/Controllers/FormularioController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FORMULARIO.Models;
+
+namespace FormMvc.Controllers
+{
+    public class FormularioController : Controller
+    {
+        private readonly ILogger<FormularioController> _logger;
+
+            public FormularioController(ILogger<FormularioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       [HttpPost]
+        public IActionResult Enviar(Formulario objFormulario){
+
+            objFormulario.Respuesta = "Ya estas Registrado" + objFormulario.Nombre;
+            return View("index", objFormulario);
+        }
+
+
+    }
+}
---
+++ FormMvc/Models/Formulario.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Formulario.Models
+{
+    public class Formulario
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string Telefono { get; set; }
+        
+        public string Subject { get; set; }
+        
+        public string Mensaje { get; set; }
+        
+        public string Respuesta { get; set; }
+    }
+}
